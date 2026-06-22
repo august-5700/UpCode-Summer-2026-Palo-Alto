221992a35646478012dcaabc7c4fff2498ebd98e
august-white <142743311+august-white@users.noreply.github.com>
2026-06-22 14:28:40 -0700

    fixed functions

--- JSONtoTSV.py
+++ JSONtoTSV.py
@@ -11,25 +11,44 @@ col_options = [
     "DP04_0089E",
     "DP04_0134E",
     "DP04_0001E",
-    "DP04_0003PE"
+    "DP04_0003PE",
+    "state",
+    "county"
 ]
 
-def get_data(cols_to_get, county = '*', state = '*'):
+def get_county_level_data(cols_to_get = "", state = '*', county = '*'):
     # verify that all cols passed are valid 
-    for col in cols_to_get:
-        if col not in col_options:
-            break
-        url = f"https://api.census.gov/data/2024/acs/acs5/profile?get={cols_to_get}&for=county:{county}&in:{state}&key={os.getenv("CENSUS_API_KEY")}" # api request url here with dates and stuff
+    if cols_to_get:
+        for col in cols_to_get:
+            if col not in col_options:
+                break
+    if state: 
+
+        
+    url = f"https://api.census.gov/data/2024/acs/acs5/profile?get={cols_to_get}&for=county:{county}&in:{state}&key={os.getenv("CENSUS_API_KEY")}" # api request url here with dates and stuff
+    response = requests.get(url)
+    return response.json()
+
+def get_block_level_data(state, county, cols_to_get = ""): # enter state and county as fip
+    # verify that all cols passed are valid 
+    if cols_to_get:
+        for col in cols_to_get:
+            if col not in col_options:
+                break
+    if state: 
+
+        
+    url = f"https://api.census.gov/data/2024/acs/acs5?get=NAME,B25077_001E,B25064_001E,B25001_001E,B25002_001E,B25002_003E&for=block%20group:*&in=state:{state}%20county:{county}%20tract:*&key={os.getenv('CENSUS_API_KEY')}" # api request url here with dates and stuff
     response = requests.get(url)
     return response.json()
 
 
 all_entries = []
-for state in range(1, 51):
+res = get_data()
+for row in res:
+    all_entries.append(row[:4])
 
-    rows = get_month_data(month, 2026)
-    all_entries += rows
-
+with open(f)
 
 def createTSV(labels): # string array of labels (county names)
     f = open('tsvfile.tsv','w')
