f8db677430fc18edde0b71ef8f387e1608048d7b
AjK-PS <166272711+lmSleepyy@users.noreply.github.com>
2026-06-22 12:32:26 -0700

    Create JSONtoTSV.py
    I think the groundwork is laid here but I doubt it'll work right away.

---
+++ JSONtoTSV.py
@@ -0,0 +1,42 @@
+import urllib.request, json
+import pandas as pd
+import requests
+import calendar
+from datetime import date
+import json
+
+
+def get_month_data(m, y):
+    start_date = date(y, m, 1)
+    _, last_day = calendar.monthrange(start_date.year, start_date.month)
+    end_of_month_date = date(start_date.year, start_date.month, last_day)
+    url = "" # api request url here with dates and stuff
+    response = requests.get(url)
+    return response.json()["features"]
+
+
+all_entries = []
+for month in range(1, 13):
+    rows = get_month_data(month, 2026)
+    all_entries += rows
+
+
+def createTSV(labels): # string array of labels
+    f = open('tsvfile.tsv','w')
+
+    toWrite = '\t'.join(labels) + '\n'
+    f.write(toWrite)
+
+    id = 0
+    for entry in all_entries:
+        toApp = f"{id}\t"
+
+        for label in labels:
+            toApp += f"{entry[label]}\t"
+
+        toApp += '\n'
+        
+        f.write(toApp)
+        id += 1
+
+    f.close()
