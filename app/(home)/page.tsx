d80e4a0a11b6e538693d3840eea70f2ff5f5e1fe
AjK-PS <166272711+lmSleepyy@users.noreply.github.com>
2026-06-30 15:23:55 -0700

    Very unpolished search but pushing anyways
    Will polish and actually make good later

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import { LeafletImporter}  from '@/components/leaflet-importer'
+import { Search } from '@/components/search'
 
 export default function Home() {
 
   return (
     <div className="relative bg-zinc-50 font-sans dark:bg-black">
       <LeafletImporter/>
+      <Search/>
     </div>
   );
 }
