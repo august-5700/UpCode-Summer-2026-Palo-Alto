a54d137f09ecfc1814f48dc5a65a594547ee3b73
AjK-PS <166272711+lmSleepyy@users.noreply.github.com>
2026-07-03 11:07:28 -0700

    Update search.tsx

--- components/search.tsx
+++ components/search.tsx
@@ -50,8 +50,13 @@ export function Search({handleSubmit}: SearchProps) {
     <Combobox items={menu}>
         <ComboboxInput placeholder="Search for a county"
         className='absolute top-4 left-6 z-[1000]
+<<<<<<< Updated upstream
         h-11 w-64 px-4
         rounded-full
+=======
+        h-10 px-3
+        rounded-3xl
+>>>>>>> Stashed changes
         border border-white/40
         bg-white/50
         text-sm text-gray-900 placeholder-gray-500
