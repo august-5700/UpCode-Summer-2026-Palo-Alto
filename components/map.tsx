30d9011d4cebcffe22bf14b4ed8cdbba3cb941fd
geltam999-hub <geltam999@gmail.com>
2026-07-08 14:57:46 -0700

    Color Gradient Changes

--- components/map.tsx
+++ components/map.tsx
@@ -27,6 +27,7 @@ interface MapProps {
 
 
 const maxZoom = 15;
+const minZoom = 2;
 const blockThreshold = 11;
 const subDivisions = 95;
 
@@ -61,13 +62,14 @@ export default function Map({ onSelectCoords, onHover, center }: MapProps) {
     // Calls when first initialized and when any movement happens, zoom/drag
     const refresh = async (map: MapType, heat: HeatLayer) => {
 
-        // Creates request id for the called refresh function
-        // Different id for each refresh call
-        const requestId = ++requestIdRef.current;
-        
-        // Grabs zoom and bounds
-        const zoom = map.getZoom();
-        const bounds = map.getBounds()
+            // Creates request id for the called refresh function
+            // Different id for each refresh call
+            const requestId = ++requestIdRef.current;
+            
+            // Grabs zoom and bounds
+            const zoom = map.getZoom();
+            if(zoom < minZoom) heatRef.current.setOptions({ zoom: minZoom });return;
+            const bounds = map.getBounds()
 
     
 
